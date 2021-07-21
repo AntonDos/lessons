4787f1d59b3a6456597a09a2542742e07da070c8
AntonDos <adosmenesk@pm.me>
2021-07-21 15:25:56 +0300

    new

--- lesson03/solution-03.py
+++ lesson03/solution-03.py
@@ -1,10 +1,3 @@
-my_list = [1, 2, 'w', 3]
-my_new_list = []
-for x in my_list:
-    new_element = str(x)
-    my_new_list.append(new_element)
-my_new_list
-my_new_list[::-1]
-''.join(my_new_list[::-1])
-"".join([str(x)] for x in my_list[::-1])
-"".join(map(my_list[::-1], str))
+list = ['w', 'r', '_', 'e', 'd', 'p']
+symbols = "".join(reversed(list))
+print(symbols)
