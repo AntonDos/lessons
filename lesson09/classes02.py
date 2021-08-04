ef543f4e58a1de291cb159c7bfad8148445af02b
AntonDos <adosmenesk@pm.me>
2021-08-04 20:57:17 +0300

    new

---
+++ lesson09/classes02.py
@@ -0,0 +1,27 @@
+#  Создать класс Dog. Класс имеет четыре атрибута: height, weight, name, age.
+#  Класс имеет три метода: jump, run, bark. Каждый метод выводит сообщение на экран.
+#  Создать объект класса Dog, вызвать все методы объекта и вывести на экран все его атрибуты.
+
+
+class Dog:
+    def __init__(self, height, weight, name, age):
+        self.height = height
+        self.weight = weight
+        self.name = name
+        self.age = age
+
+
+    def jump(self):
+        print(f"{self.name} is jumping")
+
+    def run(self):
+        print(f"{self.name} is running")
+
+    def bark(self):
+        print(f"{self.name} is barking")
+
+
+dog = Dog(height=100, weight=100, name="Bob", age=10)
+dog.jump()
+dog.run()
+dog.bark()
